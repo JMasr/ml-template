7ee26e6f614ee0e4a0c7ffa381132838202f559e
José M. Ramírez <moscaelectronica@gmail.com>
2024-01-04 20:21:50 +0100

    Test GitHub Actions Fail

--- tests/test_main_module.py
+++ tests/test_main_module.py
@@ -3,3 +3,6 @@ from src.helper_module import helper_function
 
 def test_helper_function():
     assert helper_function() == "Helper function result"
+
+def test_main_function():
+    assert main() == "Hello World"
