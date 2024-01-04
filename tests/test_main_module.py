b53c0236e1f1fcc8b31eb36b02783361e4a3bfd1
José M. Ramírez <moscaelectronica@gmail.com>
2024-01-04 20:32:11 +0100

    V1

--- tests/test_main_module.py
+++ tests/test_main_module.py
@@ -3,6 +3,3 @@ from src.helper_module import helper_function
 
 def test_helper_function():
     assert helper_function() == "Helper function result"
-
-def test_main_function():
-    assert main() == "Hello World"
